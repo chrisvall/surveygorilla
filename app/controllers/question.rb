bea895dfe1e6270844132949c63961afe5452173
chrisvall <christinevallaure@gmail.com>
2014-11-06 16:03:39 -0800

    first commit for routes

---
+++ app/controllers/question.rb
@@ -0,0 +1,8 @@
+post '/survey/:survey_id/question/new' do
+
+  #do something here for creating surveys
+  # erb :question
+  erb :question
+
+end
+
---
+++ app/controllers/survey.rb
@@ -0,0 +1,9 @@
+post '/survey/new' do
+
+  #do something here for creating surveys
+  # erb :question
+  #@survey = Survey.create(lasjdfl;asjdf;lkj)
+  erb :question
+
+end
+
